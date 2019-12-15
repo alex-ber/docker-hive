8052293e3ab47bfc02f152b6f66feb5806974c8a
alex-ber <alex-ber@users.noreply.github.com>
2019-12-15 15:12:44 +0200

    Comment added

--- run.sh
+++ run.sh
@@ -11,6 +11,10 @@ $HADOOP_PREFIX/sbin/start-yarn.sh
 err_filename=$HADOOP_HOME/logs/hiveservier2_err.log
 out_filename=$HADOOP_HOME/logs/hiveservier2_out.log
 
+# forward request and error logs to docker log collector
+#ln -sf /dev/stdout $out_filename
+#ln -sf /dev/stderr $err_filename
+
 cd $HIVE_HOME && bin/schematool -initSchema -dbType derby
 $HIVE_HOME/bin/hive --service hiveserver2 --hiveconf hive.root.logger=INFO,console </dev/null > $out_filename  2> $err_filename &
 tail -f /dev/null
